58b9045ae1ea05f0023ed81d9e634053527e64e1
Alice Nguyen <anguyen@classy.org>
2017-08-15 18:01:07 -0700

    adds sepsis-js sub dir

---
+++ sepsis-quiz-js/README.md
@@ -0,0 +1,17 @@
+## Sepsis Quiz Library for Wordpress
+
+1. build the script:
+`babel src sepsisQuiz.js --preset es2016`
+
+2. Load the script to html file
+
+3. Call `new SepsisQuiz()`
+
+
+Refer to sample.js and index.html for examples.
+
+
+
+
+
+
---
+++ sepsis-quiz-js/src/quiz.js
@@ -0,0 +1,146 @@
+class SepsisQuiz {
+  constructor(questions) {
+    this.questions = questions || []
+    this.renderedQuestions = SepsisQuiz.buildQuestions(this.questions)
+    this.score = 0
+
+    this.status = 'new'
+    this.currentQuestionIndex = 0
+
+  }
+
+  /**
+   * Returns the current question
+   * @returns {*}
+   */
+  get currentQuestion() {
+    return this.questions[this.currentQuestionIndex]
+  }
+
+  static renderChoices(choices = []) {
+    return choices.reduce((html, choice) => {
+      return `${html} <button name="choice" value=${choice} >${choice}</button>`
+
+    }, '')
+  }
+
+
+  /**
+   *
+   * @param answer
+   * @param distractors
+   * @param q
+   * @returns {Array}
+   * @private
+   */
+  static buildChoices(answer, distractors) {
+
+    let choices = []
+
+    /*all of the  above*/
+    if (typeof answer === 'boolean' && distractors instanceof Array) {
+      answer = 'all of the above'
+      choices = distractors.concat(answer)
+    }
+
+    /* true or false */
+    else if (typeof answer === 'boolean') {
+      choices = ['true', 'false']
+    }
+
+    /* multiple choice*/
+    else if (distractors instanceof Array) {
+      choices = SepsisQuiz.shuffleArray(distractors.concat(answer)) // shuffle the array
+    }
+
+    /* true or false*/
+    else if (typeof distractors === 'boolean') {
+      choices = ['true', 'false']
+    }
+
+    return choices
+  }
+
+  /**
+   * Static function to build the questions with a questions list
+   * @param questions
+   */
+  static buildQuestions(questions) {
+    if (!questions || !questions instanceof Array) {
+      return
+    }
+    return questions.map(q => {
+      q.choices = Object.assign([], SepsisQuiz.buildChoices(q.answer, q.distractors))
+      q.renderedChoices = SepsisQuiz.renderChoices(q.choices)
+
+      return q
+    })
+  }
+
+  /**
+   *  Static fucntion to shuffle an array
+   * @param arr
+   * @returns {Array.<T>}
+   */
+  static shuffleArray(arr) {
+    return arr.sort(() => (Math.random() - 0.5))
+  }
+
+  /**
+   * Finishes the quiz
+   */
+  finish() {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      throw Error(`${this.questions.length - 1 - this.currentQuestionIndex } questions left unanswered`)
+    }
+    this.status = 'finished'
+  }
+
+  /**
+   * Processes the current question and then moves onto the next question
+   * @param userInput
+   */
+  processQuestion(userInput) {
+    if (!userInput) {
+      return
+    }
+    this.score += userInput === this.currentQuestion.answer
+    this.currentQuestion.userSelected = userInput
+
+    const hasUnansweredQuestions = this.currentQuestionIndex < this.questions.length - 1
+    this.currentQuestionIndex += hasUnansweredQuestions
+
+    this.status = hasUnansweredQuestions ? 'pending' : 'finished'
+
+  }
+
+  /**
+   * Resets quiz state (ie isSubmitReady...)
+   */
+  reset() {
+    this.score = 0
+    this.status = 'new'
+    console.log('reset')
+  }
+
+  /**
+   *
+   * @param evt
+   */
+  handleOnSelect(evt) {
+    const val = evt.target.value
+    this.processQuestion(val)
+  }
+
+  handleOnNext(evt) {
+    this.currentQuestionIndex += this.currentQuestionIndex < this.questions.length - 1
+  }
+
+  handleOnPrevious(evt) {
+    this.currentQuestionIndex -= this.currentQuestionIndex > 0
+
+  }
+
+}
+
+
