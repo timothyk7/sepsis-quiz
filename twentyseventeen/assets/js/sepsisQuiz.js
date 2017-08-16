5906c2c57f6332ce1c5c23630e95a2efd3e85b7d
Alice Nguyen <anguyen@classy.org>
2017-08-15 17:54:27 -0700

    project cleanup

--- twentyseventeen/assets/js/sepsisQuiz.js
+++ twentyseventeen/assets/js/sepsisQuiz.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -125,18 +127,18 @@ var SepsisQuiz = function () {
 
       /* true or false */
       else if (typeof answer === 'boolean') {
-          choices = ['true', 'false'];
-        }
+        choices = ['true', 'false'];
+      }
 
-        /* multiple choice*/
-        else if (distractors instanceof Array) {
-            choices = SepsisQuiz.shuffleArray(distractors.concat(answer)); // shuffle the array
-          }
+      /* multiple choice*/
+      else if (distractors instanceof Array) {
+        choices = SepsisQuiz.shuffleArray(distractors.concat(answer)); // shuffle the array
+      }
 
-          /* true or false*/
-          else if (typeof distractors === 'boolean') {
-              choices = ['true', 'false'];
-            }
+      /* true or false*/
+      else if (typeof distractors === 'boolean') {
+        choices = ['true', 'false'];
+      }
 
       return choices;
     }
@@ -177,4 +179,3 @@ var SepsisQuiz = function () {
 
   return SepsisQuiz;
 }();
-
