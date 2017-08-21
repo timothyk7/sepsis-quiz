9c7a2133aae578b92bf161b50c1cd0ba9d089b40
Alice Nguyen <anguyen@classy.org>
2017-08-20 17:18:02 -0700

    fixes processing all of the above questions

--- sepsis-quiz-js/src/sepsisQuiz.js
+++ sepsis-quiz-js/src/sepsisQuiz.js
@@ -1,3 +1,6 @@
+const allOfTheAbove = 'All of the above'
+const noneOfTheAbove = 'None of the above'
+
 class SepsisQuiz {
   constructor(questions) {
     this.questions = SepsisQuiz.buildQuestions(questions)
@@ -37,7 +40,7 @@ class SepsisQuiz {
   }
 
   renderShareBlock(tweet) {
-    var twitterLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweet.text)}${tweet.url ? `&url=${encodeURIComponent(tweet.url)}` : ''}`;
+    var twitterLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweet.text)}${tweet.url ? `&url=${encodeURIComponent(tweet.url)}` : ''}`
     return `
     <div class="under-card-top"></div>
     <div class="card-container">
@@ -56,9 +59,8 @@ class SepsisQuiz {
           </div>
         </div>
       </div>
-    `;
+    `
   }
-
 
   /**
    *
@@ -74,7 +76,7 @@ class SepsisQuiz {
 
     /*all of the  above*/
     if (typeof answer === 'boolean' && wrongAnswers instanceof Array) {
-      answer = 'All of the above.'
+      answer = allOfTheAbove
       choices = SepsisQuiz.shuffleArray(wrongAnswers).concat(answer)
     }
 
@@ -83,7 +85,7 @@ class SepsisQuiz {
       choices = SepsisQuiz.shuffleArray(wrongAnswers.concat(answer)) // shuffle the array
 
       // Hack to deal with our one none of the above (it's not the answer!)
-      const none = 'None of the above.'
+      const none = noneOfTheAbove
       const idx = choices.indexOf(none)
       if (idx > -1) {
         choices.splice(idx, 1)
@@ -107,7 +109,7 @@ class SepsisQuiz {
     if (!questions || !(questions instanceof Array)) {
       return []
     }
-    return questions.map((q, index )=> {
+    return questions.map((q, index) => {
       q.id = index
       q.choices = SepsisQuiz.buildChoices(q.answer, q.wrongAnswers) || []
       q.renderedChoices = SepsisQuiz.renderChoices(q.choices, index)
@@ -115,7 +117,6 @@ class SepsisQuiz {
       return q
     })
   }
-
 
   /**
    *  Static fucntion to shuffle an array
@@ -132,199 +133,191 @@ class SepsisQuiz {
    */
   processQuestion(questionIndex, userInput) {
     const question = this.questions[questionIndex]
+
     if (!userInput || question.userSelected !== undefined) {
       return null
     }
+    const expectedAnswer = typeof question.answer === 'boolean' ? question.answer ? allOfTheAbove : noneOfTheAbove : question.answer
+    const isCorrect = userInput === expectedAnswer
 
-    const isCorrect = userInput === this.questions[questionIndex].answer
     this.score += isCorrect
     this.questions[questionIndex].userSelected = userInput
 
-    // todo remoeeeee
-    console.log(this.score, this.questions[questionIndex])
-
     return isCorrect
-
   }
 
 }
 
-
-
-
-
-
 // Loads code on screen
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
   var questions = [
     {
-      questionText: "About how many people in the U.S. die each year because of sepsis?",
+      questionText: 'About how many people in the U.S. die each year because of sepsis?',
       wrongAnswers: [
-        "45,000",
-        "1,200,000",
-        "10,000",
+        '45,000',
+        '1,200,000',
+        '10,000',
       ],
-      answer: "258,000",
+      answer: '258,000',
       learnMore: {
-        text: "Every two minutes, a life is lost to sepsis in the U.S., totaling over a quarter million people every year. That number jumps to an estimated 8 million across the globe.",
-        link: "http://www.sepsis.org/resources/diagnosed-with-sepsis/",
+        text: 'Every two minutes, a life is lost to sepsis in the U.S., totaling over a quarter million people every year. That number jumps to an estimated 8 million across the globe.',
+        link: 'http://www.sepsis.org/resources/diagnosed-with-sepsis/',
       },
     },
     {
-      questionText: "What is sepsis?",
+      questionText: 'What is sepsis?',
       wrongAnswers: [
-        "A local infection, such as cellulitis or appendicitis.",
-        "An infection in the blood.",
-        "A contagious disease.",
+        'A local infection, such as cellulitis or appendicitis.',
+        'An infection in the blood.',
+        'A contagious disease.',
       ],
-      answer: "Your body's toxic response to an infection.",
+      answer: 'Your body\'s toxic response to an infection.',
       learnMore: {
-        text: "More than 40% of Americans have never heard the word sepsis. It’s your body’s extreme and toxic response to an infection. It's life threatening and, without the right treatment, can cause organ failure, amputation, and death.",
-        link: "http://www.sepsis.org/sepsis/definition/",
+        text: 'More than 40% of Americans have never heard the word sepsis. It’s your body’s extreme and toxic response to an infection. It\'s life threatening and, without the right treatment, can cause organ failure, amputation, and death.',
+        link: 'http://www.sepsis.org/sepsis/definition/',
       },
     },
     {
-      questionText: "Sepsis can develop from:",
+      questionText: 'Sepsis can develop from:',
       wrongAnswers: [
-        "A cut on your finger.",
-        "A mosquito bite.",
-        "A tattoo.",
-        "A urinary tract infection (UTI).",
+        'A cut on your finger.',
+        'A mosquito bite.',
+        'A tattoo.',
+        'A urinary tract infection (UTI).',
       ],
       answer: true,
       learnMore: {
-        text: "As many as 92% of sepsis cases come from the community, not the hospital. That means sepsis can develop from any type of infection including a UTI, strep throat, flu, pneumonia, and more. In fact, any time your body has a break in the skin, like from a cut or even a piercing, there's a chance it could cause an infection. Preventing and treating infections as soon as they develop are key to helping prevent sepsis.",
-        link: "http://www.sepsis.org/sepsis-and/",
+        text: 'As many as 92% of sepsis cases come from the community, not the hospital. That means sepsis can develop from any type of infection including a UTI, strep throat, flu, pneumonia, and more. In fact, any time your body has a break in the skin, like from a cut or even a piercing, there\'s a chance it could cause an infection. Preventing and treating infections as soon as they develop are key to helping prevent sepsis.',
+        link: 'http://www.sepsis.org/sepsis-and/',
       },
     },
     {
-      questionText: "All of the following are signs of sepsis EXCEPT:",
+      questionText: 'All of the following are signs of sepsis EXCEPT:',
       wrongAnswers: [
-        "Fever or feeling chilled.",
-        "Confusion/difficult to arouse.",
+        'Fever or feeling chilled.',
+        'Confusion/difficult to arouse.',
         'Extreme pain or discomfort ("worst ever").',
-        "Rapid breathing.",
+        'Rapid breathing.',
       ],
-      answer: "Slow heart rate.",
+      answer: 'Slow heart rate.',
       learnMore: {
-        text: "Less than 1% of Americans can correctly name all the common signs of sepsis, one of which is a rapid heart rate as your heart works to pump blood through your body. You can save a life just by arming yourself by knowing the signs of sepsis.",
-        link: "http://www.sepsis.org/sepsis/symptoms/",
+        text: 'Less than 1% of Americans can correctly name all the common signs of sepsis, one of which is a rapid heart rate as your heart works to pump blood through your body. You can save a life just by arming yourself by knowing the signs of sepsis.',
+        link: 'http://www.sepsis.org/sepsis/symptoms/',
       },
     },
     {
-      questionText: "Who is at highest risk for developing sepsis?",
+      questionText: 'Who is at highest risk for developing sepsis?',
       wrongAnswers: [
-        "Newborn babies.",
-        "People with cancer.",
-        "People over 65 years old.",
+        'Newborn babies.',
+        'People with cancer.',
+        'People over 65 years old.',
       ],
       answer: true,
       learnMore: {
-        text: "Anyone can develop sepsis, no matter how healthy they are. However, it's especially risky for those with weaker immune systems.",
-        link: "http://www.sepsis.org/sepsis/risk-factors/",
+        text: 'Anyone can develop sepsis, no matter how healthy they are. However, it\'s especially risky for those with weaker immune systems.',
+        link: 'http://www.sepsis.org/sepsis/risk-factors/',
       },
     },
     {
-      questionText: "When someone has severe sepsis, their chances of survival drop by almost 8% for every _____ that goes by without treatment.",
+      questionText: 'When someone has severe sepsis, their chances of survival drop by almost 8% for every _____ that goes by without treatment.',
       wrongAnswers: [
-        "Minute.",
-        "Day.",
-        "None of the above.",
+        'Minute.',
+        'Day.',
+        'None of the above.',
       ],
-      answer: "Hour.",
+      answer: 'Hour.',
       learnMore: {
-        text: "Sepsis can be treated if it's identified early, which prevents it from progressing and leading to extreme consequences like amputation or death. Getting medical attention right away if you suspect sepsis is as important as treating heart attacks and strokes quickly.",
-        link: "http://www.sepsis.org/sepsis/treatment/",
+        text: 'Sepsis can be treated if it\'s identified early, which prevents it from progressing and leading to extreme consequences like amputation or death. Getting medical attention right away if you suspect sepsis is as important as treating heart attacks and strokes quickly.',
+        link: 'http://www.sepsis.org/sepsis/treatment/',
       },
     },
     {
-      questionText: "Adults older than 65 are _____ times more likely to be hospitalized with sepsis than adults younger than 65.",
+      questionText: 'Adults older than 65 are _____ times more likely to be hospitalized with sepsis than adults younger than 65.',
       wrongAnswers: [
-        "5",
-        "20",
-        "27",
+        '5',
+        '20',
+        '27',
       ],
-      answer: "13",
+      answer: '13',
       learnMore: {
-        text: "Did you know sepsis is the most costly condition billed to Medicare? As people age, their immune systems can't fight off infections as easily, making them at greater risk for developing sepsis. Mary Beth West was 72 when she survived sepsis from a UTI, read her story.",
-        link: "http://www.sepsis.org/faces/mary-beth-west/",
+        text: 'Did you know sepsis is the most costly condition billed to Medicare? As people age, their immune systems can\'t fight off infections as easily, making them at greater risk for developing sepsis. Mary Beth West was 72 when she survived sepsis from a UTI, read her story.',
+        link: 'http://www.sepsis.org/faces/mary-beth-west/',
       },
     },
     {
-      questionText: "Every day, an average of _____ amputations occur because of sepsis.",
+      questionText: 'Every day, an average of _____ amputations occur because of sepsis.',
       wrongAnswers: [
-        "10",
-        "52",
-        "29",
+        '10',
+        '52',
+        '29',
       ],
-      answer: "38",
+      answer: '38',
       learnMore: {
-        text: "Unfortunately, amputation is a very real consequence of sepsis. Blockages inside the blood vessels cause the body's tissue to die which can require amputation. Michael Stolzenberg was 8 years-old when sepsis led to amputation to both his arms and legs. Watch Michael's inspiring story.",
-        link: "http://www.sepsis.org/erin/",
+        text: 'Unfortunately, amputation is a very real consequence of sepsis. Blockages inside the blood vessels cause the body\'s tissue to die which can require amputation. Michael Stolzenberg was 8 years-old when sepsis led to amputation to both his arms and legs. Watch Michael\'s inspiring story.',
+        link: 'http://www.sepsis.org/erin/',
       },
     },
     {
-      questionText: "Sepsis symptoms can be different for children and adults. Which of the below is a symptom of sepsis in a child?",
+      questionText: 'Sepsis symptoms can be different for children and adults. Which of the below is a symptom of sepsis in a child?',
       wrongAnswers: [
-        "High fever (above 100.4 degrees).",
-        "General illness or a previous injury, such as a scrape or cut.",
-        "Very fast or rapid breathing.",
-        "Lethargy or difficulty waking up.",
+        'High fever (above 100.4 degrees).',
+        'General illness or a previous injury, such as a scrape or cut.',
+        'Very fast or rapid breathing.',
+        'Lethargy or difficulty waking up.',
       ],
       answer: true,
       learnMore: {
-        text: "Sepsis in children is a problem - more than 75,000 children develop severe sepsis each year in the U.S. and many have lasting complications. If a child has a combination of any of these symptoms, it's important to get medical attention right away. Best rule of thumb? When in doubt, check with your doctor or bring your child to the emergency room for evaluation.",
-        link: "http://www.sepsis.org/sepsis-and/children/",
+        text: 'Sepsis in children is a problem - more than 75,000 children develop severe sepsis each year in the U.S. and many have lasting complications. If a child has a combination of any of these symptoms, it\'s important to get medical attention right away. Best rule of thumb? When in doubt, check with your doctor or bring your child to the emergency room for evaluation.',
+        link: 'http://www.sepsis.org/sepsis-and/children/',
       },
     },
     {
-      questionText: "Which of the following is NOT likely to be a complication after surviving sepsis?",
+      questionText: 'Which of the following is NOT likely to be a complication after surviving sepsis?',
       wrongAnswers: [
-        "Insomnia.",
-        "Post-traumatic stress disorder (PTSD).",
-        "Decreased mental functioning.",
-        "Amputations.",
+        'Insomnia.',
+        'Post-traumatic stress disorder (PTSD).',
+        'Decreased mental functioning.',
+        'Amputations.',
       ],
-      answer: "Improved memory.",
+      answer: 'Improved memory.',
       learnMore: {
-        text: "There are more than 1.6 million cases of sepsis every year and survivors often face long-term effects, also known as post-sepsis syndrome, including amputations, anxiety, memory loss, chronic pain and fatigue, and more.",
-        link: "http://www.sepsis.org/life-after-sepsis/",
+        text: 'There are more than 1.6 million cases of sepsis every year and survivors often face long-term effects, also known as post-sepsis syndrome, including amputations, anxiety, memory loss, chronic pain and fatigue, and more.',
+        link: 'http://www.sepsis.org/life-after-sepsis/',
       },
     },
-  ];
+  ]
 
-  var sepsisQuiz = new SepsisQuiz(questions);
+  var sepsisQuiz = new SepsisQuiz(questions)
 
   function render(sepsisQuiz) {
     /* ------ status ------ */
 
     /* ------ questions ------ */
-    $('#questions_container').html(sepsisQuiz.renderedQuestions);
+    $('#questions_container').html(sepsisQuiz.renderedQuestions)
     $('input.choice').bind('click', onSelect)
 
     /* ------ share ------ */
     var tweet = {
-      text: "Share Awareness",
-      url: "http://www.sepsis.org/"
+      text: 'Share Awareness',
+      url: 'http://www.sepsis.org/'
     }
-    $('#share_container').html(sepsisQuiz.renderShareBlock(tweet));
+    $('#share_container').html(sepsisQuiz.renderShareBlock(tweet))
   }
 
-
-  render(sepsisQuiz);
+  render(sepsisQuiz)
 
   /* ------ event handlers ------ */
   function onSelect(e) {
     const qId = $(e.target).attr('data-question-id')
     const val = e.target.value
 
-    const res  = sepsisQuiz.processQuestion(qId, val)
-    
+    const res = sepsisQuiz.processQuestion(qId, val)
+
     if (res !== null) {
-      $(e.target).addClass( res ? 'correct' : 'incorrect' )
+      $(e.target).addClass(res ? 'correct' : 'incorrect')
     }
   }
 
-  console.log('quiz:', sepsisQuiz);
-});
+  console.log('quiz:', sepsisQuiz)
+})
